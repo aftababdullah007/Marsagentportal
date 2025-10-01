8ea637b24360a247bef31807e3eb053919bf4564
Sohrab Khan <sohrabprofile@gmail.com>
2025-10-02 01:53:40 +0500

    Update ALLOWED_HOSTS to allow all hosts in settings.py

--- mars_portal/settings.py
+++ mars_portal/settings.py
@@ -4,7 +4,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'replace-this-in-production'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
